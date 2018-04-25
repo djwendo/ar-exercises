6c59798e3600ae2d215fd95dd2a04b09e5d152fa
Wendy Braun <wendocalrissian@gmail.com>
2018-04-25 02:04:36 +0000

    Complete exercise_9 adding before_destroy callback

--- lib/store.rb
+++ lib/store.rb
@@ -1,13 +1,23 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  before_destroy :check_employees
   validates :name, presence: true, length: {minimum: 3}
   validates :annual_revenue, presence: true, numericality: {greater_than_or_equal_to: 0}
   validate :must_carry_mens_or_womens_apparel
+
+  
 
   def must_carry_mens_or_womens_apparel
     if !womens_apparel && !mens_apparel
       errors.add(:apparel_type, "error: stores must carry at least one of men's or women's apparel")
     end
   end
+
+  private
+    def check_employees
+      if employees.size != 0
+        raise "Destroy aborted!"
+      end
+    end
 
 end
